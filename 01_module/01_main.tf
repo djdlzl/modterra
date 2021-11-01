140226a4f1d29b3cc9f91743375c61f74cbb201d
jwcho <djdlzl93@gmail.com>
2021-11-01 12:02:58 +0900

    directory2

---
+++ 01_module/01_main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+
+resource "aws_key_pair" "jwcho_key" {
+  key_name = "tf-key1"
+  public_key = file("../../.ssh/id_rsa.pub")
+}
+
+
+
---
+++ 01_module/11_web.tf
@@ -0,0 +1,38 @@
+data "aws_ami" "amzn" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "jwcho_weba" {
+  ami                      = data.aws_ami.amzn.id
+  instance_type            = "t2.micro"
+  key_name                 = "tf-key"
+  vpc_security_group_ids   = [aws_security_group.jwcho_websg.id]
+  availability_zone        = "ap-northeast-2a"
+  subnet_id                = aws_subnet.jwcho_puba.id
+  associate_public_ip_address = true
+  user_data                = file("install_apache.sh")
+  tags = {
+    "Name" = "jwcho-weba"
+  }
+}
+/*
+resource "aws_eip" "jwcho_web_eip" {
+  vpc = true
+  instance  = aws_instance.jwcho_weba
+  associate_with_private_ip = "10.0.0.11"
+  depends_on = [aws_internet_gateway.jwcho_igw
+  ]
+}
+*/
