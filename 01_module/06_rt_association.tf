85dee67fd262ab858694fc4bad9cb1ad33428b06
Jaewoo Cho <75625720+djdlzl@users.noreply.github.com>
2021-11-01 21:31:59 +0900

    Delete 01_module directory

--- 01_module/06_rt_association.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "jwcho_rtas_a" {
-  subnet_id = aws_subnet.jwcho_puba.id
-  route_table_id = aws_route_table.jwcho_rt.id
-}
-
-resource "aws_route_table_association" "jwcho_rtas_c" {
-  subnet_id = aws_subnet.jwcho_pubc.id
-  route_table_id = aws_route_table.jwcho_rt.id
-}
--- 01_module/15_lbtg_atcm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_lb_target_group_attachment" "jwcho_lbtg_att" {
-  target_group_arn = aws_lb_target_group.jwcho_lb_tg.arn
-  target_id        = aws_instance.jwcho_weba.id
-  port             = 80
-}
